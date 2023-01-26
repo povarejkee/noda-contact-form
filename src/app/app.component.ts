4996c45c5e759a246063e7ecfd675c2786e806a8
i.popov <i.popov@sdventures.com>
2023-01-26 12:06:43 +0400

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'noda-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'noda-contact-form';
+}
